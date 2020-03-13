95cbe43846c7fe7b2c891441c539875b93c3e3ce
Fernando <fernando.bragga@gmail.com>
2020-03-13 20:37:57 +1100

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false}, #username needs to be unique regardless of capital letters
+            length: {minimum: 3, maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #contain the regular expression to validate emails
+
+  validates :email, presence: true,
+            length: {maximum: 150},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX } #VALID_EMAIL_REGEX works as a "variable"
+
 end
