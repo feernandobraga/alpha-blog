8d5bbd7c96fc80c0929a75cd0a322c7860cdc1ec
Fernando <fernando.bragga@gmail.com>
2020-03-14 11:05:47 +1100

    fixing gitignore

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 
+  belongs_to :user # it means that any article belongs to a single user
+
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 5}
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :articles #indicates that one user can have many articles
+
   validates :username, presence: true,
             uniqueness: {case_sensitive: false}, #username needs to be unique regardless of capital letters
             length: {minimum: 3, maximum: 50}
